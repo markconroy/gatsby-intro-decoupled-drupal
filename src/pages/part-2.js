713fb3e48d1fe202e518f673bef19e4ec7a30eaf
Mark Conroy <info@adesignforlife.net>
2019-05-09 20:52:22 +0100

    update class name to export

--- src/pages/part-2.js
+++ src/pages/part-2.js
@@ -3,7 +3,7 @@ import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
+const Page2Page = () => (
   <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
       <h1>What's Changed Here?</h1>
@@ -18,4 +18,4 @@ const IndexPage = () => (
   </Layout>
 )
 
-export default IndexPage
+export default Page2Page
