45779390cc049477359af29a57cb1752a4c98711
Mark Conroy <info@adesignforlife.net>
2019-05-09 22:00:38 +0100

    updating site title

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: `GatsbyJS Intro: Part 2`,
+    title: `GatsbyJS Intro: Part 3`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
     author: `@markconroy`,
   },
